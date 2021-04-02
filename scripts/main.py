b820088f355c5eda1df730a62edc723e453c388d
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-04-01 19:10:39 -0500

    updated main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from astropy.timeseries import LombScargle
 
@@ -18,8 +19,121 @@ class RV_Detection(object):
        Array of velocities.
     err : np.ndarray
        Array of standard errors.
+
+    Attributes
+    ----------
+    df : pandas.core.frame.DataFrame
+       Table of input parameters.
     """
     
     def __init__(self, time, vel, err):
+        df = pd.DataFrame(columns=['Time', 'Vel', 'Err'])
+        df['Time'] = time
+        df['Vel'] = vel
+        df['Err'] = err
+        self.df = df
+        self.peak_period = None
+
+
+     def lomb_scargle(self, minfreq=1.0/50., maxfreq=1.0/0.5):
+        """
+        Creates Lomb-Scargle periodogram for RV data within a given frequency 
+        range.
+        
+        Parameters
+        ----------
+        minfreq : float, optional
+            Minimum frequency to compute over. Default = 1/50.
+        maxfreq : float, optional
+            Maximum frequency to compute over. Default = 1/0.5.
+            
+        Attributes
+        ----------
+        peak_period : float
+            Peak period in periodogram.
+        LS_results : np.ndarray
+            Results array from astropy.timeseries.LombScarlge.autopower().
+        self.all_P : np.ndarray
+            Array of length 10000 spanning the minimum and maximum input
+            frequencies.
+        """
+        results = LombScargle(self.df['Time'], self.df['Vel'],
+                              dy=self.df['Err']).autopower(minimum_frequency=minfreq,
+                                                           maximum_frequency=maxfreq,
+                                                           samples_per_peak=50.0)
+        argmax = np.argmax(results[1])
+        self.peak_period = 1.0/results[0][argmax]
+        self.LS_results = results
+        self.all_P = 10**(np.linspace(np.log10(maxfreq), 
+                                      np.log10(minfreq), 10000))
+    
+
+     def get_X(self, x):
+        """
+        Converts RV data frame into a matrix of covariates.
+        """
+        X0 = np.stack((np.ones(len(x)), x), axis=1)
+        p = len(self.df.columns) - 3 # Number of covariate signals
+        # Didn't implement if p > 0
+        return X0
+
+
+     def fit_weighted(self, add_P=np.array([])):
+        """
+        Fits the RV data.
+        
+        Parameters
+        ----------
+        add_P : np.ndarray, optional
+            Array of periods which petermines which periodic terms to include.
+        """
+        ts = self.df['Time']
+        y = self.df['Vel']
+        se = self.df['Err']
+        X0 = self.get_X(ts)
+        
+        # Adds periodic terms if passed in
+        if len(add_P) > 0:
+            new_X0 = np.zeros((len(ts), len(np.array(add_P))+X0.shape[1]))
+            new_X0[:,0] = X0[:,0]
+            new_X0[:,1] = X0[:,1]
+            
+            c = 2
+            for j in range(len(np.array(add_P))):
+                new_X0[:,c] = np.cos(2 * np.pi * ts)/add_P[j]
+                new_X0[:,c+1] = np.sin(2 * np.pi * ts)/add_P[j]
+                c += 2
+        
+        def loss_function(params):
+            """
+            Quadratic loss function.
+            """
+            nonlocal se
+            s = np.exp(params[1]) # sigma parameter
+            v = s**2 + se**2
+            ####
+            # STOPPED HERE FOR THE NIGHT OF 01-04-201
+            ####
+        
+        best = least_squares()
 
     
+    def null_periodogram(self, theta0, null_samples=100, verbose=False):
+        """
+        Samples the periodogram under the null hypothesis that
+        $\theta$* = $\theta$0.
+        """
+        n = len(self.df)
+        fit = self.fit_weighted(add_P=theta0)
+        ## COME BACK HERE AFTER FIT_WEIGHTED DONE
+        
+        
+    def test_period_theta0(self, theta0, all_p=None, null_samples=100, ls0=None):
+        if all_p == None:
+            all_p=self.all_P
+            
+        if self.peak_period == None:
+            self.lomb_scargle()
+            
+        id0 = np.argmin(np.abs(all_p - theta0))
+        Pf_null = self.null_periodogram(theta0=theta0, null_samples=null_samples)
