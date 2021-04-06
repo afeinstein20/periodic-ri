2c102e7e5a0fea74b0e2f7ac4ba4612d37461e68
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-04-06 10:32:17 -0500

    finish up null_periodogram function

--- scripts/main.py
+++ scripts/main.py
@@ -37,40 +37,52 @@ class RV_Detection(object):
         self.peak_period = None
 
 
-     def lomb_scargle(self, minfreq=1.0/50., maxfreq=1.0/0.5):
-        """
-        Creates Lomb-Scargle periodogram for RV data within a given frequency 
-        range.
+    def lomb_scargle(self, y=None, minfreq=1.0/50., maxfreq=1.0/0.5, ret_results=False):
+         """
+         Creates Lomb-Scargle periodogram for RV data within a given frequency 
+         range.
         
-        Parameters
-        ----------
-        minfreq : float, optional
+         Parameters
+         ----------
+         minfreq : float, optional
             Minimum frequency to compute over. Default = 1/50.
-        maxfreq : float, optional
+         maxfreq : float, optional
             Maximum frequency to compute over. Default = 1/0.5.
             
-        Attributes
-        ----------
-        peak_period : float
+         Attributes
+         ----------
+         peak_period : float
             Peak period in periodogram.
-        LS_results : np.ndarray
+         LS_results : np.ndarray
             Results array from astropy.timeseries.LombScarlge.autopower().
-        self.all_P : np.ndarray
+         self.all_P : np.ndarray
             Array of length 10000 spanning the minimum and maximum input
             frequencies.
-        """
-        results = LombScargle(self.df['Time'], self.df['Vel'],
-                              dy=self.df['Err']).autopower(minimum_frequency=minfreq,
-                                                           maximum_frequency=maxfreq,
-                                                           samples_per_peak=50.0)
-        argmax = np.argmax(results[1])
-        self.peak_period = 1.0/results[0][argmax]
-        self.LS_results = results
-        self.all_P = 10**(np.linspace(np.log10(maxfreq), 
-                                      np.log10(minfreq), 10000))
+         """
+         if y is None:
+             y = self.df['Vel']+0.0
+
+         results = LombScargle(self.df['Time'], y,
+                               dy=self.df['Err']).autopower(minimum_frequency=minfreq,
+                                                            maximum_frequency=maxfreq,
+                                                            samples_per_peak=50.0)
+         argmax = np.argmax(results[1])
+         all_P = 10**(np.linspace(np.log10(maxfreq),
+                                  np.log10(minfreq), 10000))
+         
+         if ret_results == False:
+             self.peak_period = 1.0/results[0][argmax]
+             self.LS_results = results
+             self.all_P = all_P + 0.0
+
+         else:
+             return {'periods':1.0/results[0],
+                     'power':results[1],
+                     'peak_period':1.0/results[0][argmax],
+                     'all_P': all_P}
     
 
-     def get_X(self, x):
+    def get_X(self, x):
         """
         Converts RV data frame into a matrix of covariates.
         """
@@ -80,7 +92,7 @@ class RV_Detection(object):
         return X0
 
 
-     def fit_weighted(self, add_P=np.array([])):
+    def fit_weighted(self, add_P=np.array([])):
         """
         Fits the RV data.
         
@@ -96,12 +108,12 @@ class RV_Detection(object):
         
         # Adds periodic terms if passed in
         if len(add_P) > 0:
-            new_X0 = np.zeros((len(ts), len(np.array(add_P))+X0.shape[1]))
+            new_X0 = np.zeros((len(ts), len(np.array(add_P))*2+X0.shape[1]))
             new_X0[:,0] = X0[:,0]
             new_X0[:,1] = X0[:,1]
             
             c = 2
-            for j in range(len(np.array(add_P))):
+            for j in range(len(add_P)):
                 new_X0[:,c] = np.cos(2 * np.pi * ts)/add_P[j]
                 new_X0[:,c+1] = np.sin(2 * np.pi * ts)/add_P[j]
                 c += 2
@@ -124,9 +136,14 @@ class RV_Detection(object):
         
             if ret_logL == False:
                 yhat = y - res
-                return(np.sqrt(v), fit, yhat, -logL,
-                       np.correlate(y, yhat)**2, shape,
-                       params)
+                ret_dict = {'sigma':np.sqrt(v),
+                            'bhat':fit,
+                            'yhat':yhat,
+                            'logL':-logL,
+                            'R2':np.correlate(y, yhat)**2,
+                            'df':shape,
+                            'params':params}
+                return ret_dict
             
             else:
                 return -logL # log likelihood
@@ -145,11 +162,41 @@ class RV_Detection(object):
         """
         Samples the periodogram under the null hypothesis that
         $\theta$* = $\theta$0.
+
+        Returns
+        -------
+        all_power : np.ndarray
+           Array of Lomb Scargle power values per each null sample.
+        all_P : np.ndarray
+           Array of periods sampled.
+        theta0 : float
         """
+
         n = len(self.df)
-        fit = self.fit_weighted(add_P=theta0)
-        ## COME BACK HERE AFTER FIT_WEIGHTED DONE
+
+        # 1. Fit y(t) ~ X + Per(theta0, t) + eta(t)
+        fit = self.fit_weighted(add_P=np.array([theta0]))
+        Yhat0 = fit['yhat']
+
+        # Residuals under H0
+        e0 = self.df['Vel'] - Yhat0
+        signs = np.append(np.full(int(n/2), -1), np.full(int(n/2), 1))
+        if len(signs) < n:
+            signs = np.append(signs, np.full(int(n-len(signs)), 1))
+
+
+        # Only assume symmetric errors
+        for i in range(null_samples):
+            e = np.random.choice(signs, size=len(signs), replace=False) * e0
+            y_new = Yhat0 + e
+
+            # 2. Lomb-Scargle periodogram
+            out = self.lomb_scargle(y=y_new, ret_results=True)
+            if i == 0:
+                 all_power = np.zeros((null_samples, len(out['power'])))
+            all_power[i] = out['power']
         
+        return all_power, out['all_P'], theta0
         
     def test_period_theta0(self, theta0, all_p=None, null_samples=100, ls0=None):
         if all_p == None:
@@ -160,3 +207,6 @@ class RV_Detection(object):
             
         id0 = np.argmin(np.abs(all_p - theta0))
         Pf_null = self.null_periodogram(theta0=theta0, null_samples=null_samples)
+        #######
+        # return here when null_periodogram is complete
+        #######
